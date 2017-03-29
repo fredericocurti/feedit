e30871a318ef54237c780e63ec9f826bb0dc4c1f
Frederico Vilela Curti <frederico.vilelacurti@gmail.com>
2017-03-29 00:51:20 -0300

    inital

---
+++ js/firebase.js
@@ -0,0 +1,29 @@
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-2.1.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+var config = {
+  apiKey: "AIzaSyBcFce_uWpOt8QHJKnU7fBBaPmOSMEbiTo",
+  authDomain: "febee-2b942.firebaseapp.com",
+  databaseURL: "https://febee-2b942.firebaseio.com",
+  storageBucket: "febee-2b942.appspot.com",
+  messagingSenderId: "301542026521"
+};
+
+function writeUserData(userId, name, email) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+  });
+}
+
+function retrieve(){
+  $("#submitbt").hide();
+  $("#loadingbar").show();
+  return firebase.database().ref('/users/oi').once('value').then(function(snapshot) {
+    var username = snapshot.val().email;
+    // document.write(username)
+    // ...
+  });
+}
