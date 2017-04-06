6a617cc95c787a40cb5453ef49f66529dd0e4b6f
Frederico Vilela Curti <frederico.vilelacurti@gmail.com>
2017-04-05 22:13:02 -0300

    added counters

--- public/js/main.js
+++ public/js/main.js
@@ -8,121 +8,11 @@ var config = {
 };
 var places = [];
 var updatecounter = 0;
-var initalstate = 0;
+var initialstate = 0;
 var newEval = null;
 var datacounter = 0;
 var db = {};
-
-console.log("update counter:"+updatecounter);
-
-// Initialize mainapp
-Feedit.prototype.displayGui = function(){
-  $("#header").remove();
-  $("#iconsection").remove();
-  $("#mainshow").removeClass("mainbg");
-  $("#main-content").css("height","100%");
-  $( "#main-content" ).append(
-  "<div class='center'><h5><b>Painel de Controle</b></h5><h6 style='color:grey;'>" + currentUser.email + " | ID: " + currentUser.uid +  "</h6>" +
-  "<div class='divider'></div></div>"+
-  "<div id='maindata'></div>");
-  $("#navbar").css("background-color","rgb(187,41,41)");
-  $("#loadingbar").attr("style","");
-  loading = document.getElementById("loadingbar");
-  this.getData();
-  // Draws container for key values
-  $("#maindata").append(
-    '<div class="container">'+
-    '<h5 style="color:gray">Seus locais</h5>'+
-    '<ul id="main-values" class="collapsible" data-collapsible="expandable">'+
-    '</ul>'+
-    '</div>');
-}
-
-Feedit.prototype.displayData2 = function(data){
-  // console.log("display data called | updatecounter:"+updatecounter);
-  Key = data;
-    if(places.includes(Key.local) == true){
-      console.log("locale "+Key.local+" already exists. Appending to the correct place")
-      var localid = '#'+Key.local
-      $(localid).append(
-      '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'
-      );
-    } else {
-      $("#main-values").append(
-        '<li id="'+Key.local+'">'+
-        '  <div class="collapsible-header" id="'+Key.local+'"><i class="material-icons">label_outline</i>'+ Key.local.capitalize() + '</div>'+
-        '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'+
-        '</li>');
-    }
-    places.push(Key.local);
-    $("#loadingbar").remove();
-    console.log("Initial data written to UI successfully");
-    $('.collapsible').collapsible();
-    // Adds aditional data loaded in realtime
-    for(i=0;i<places.removeDuplicates().length;i++){
-        $('.collapsible').collapsible('open',i);
-    }
-}
-
-
-
-Feedit.prototype.displayData = function(kind){
-  console.log("display data called | updatecounter:"+updatecounter);
-  if(kind==0){
-    keyplaces = []
-      for (key in initialdb) {
-        if (initialdb.hasOwnProperty(key)) {
-          // Check for keys that contain different locals;
-          Key = initialdb[key]
-          // console.log(Key);
-          if(keyplaces.includes(Key.local) == true){
-            console.log("locale "+Key.local+" already exists. Appending to the correct place")
-            var localid = '#'+Key.local
-            $(localid).append(
-              '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'
-              );
-            } else {
-              $("#main-values").append(
-                '<li id="'+Key.local+'">'+
-                '  <div class="collapsible-header"><i class="material-icons">label_outline</i>'+ Key.local.capitalize() + '</div>'+
-                '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'+
-                '</li>');
-            }
-            keyplaces.push(Key.local);
-        }
-    }
-    $("#loadingbar").remove();
-    console.log("Initial data written to UI successfully");
-    $('.collapsible').collapsible();
-    // Adds aditional data loaded in realtime
-  } else if (kind==1 && updatecounter>=1){
-    console.log("updatecounter >= 1!");
-    Key = newEval;
-    if(keyplaces.includes(Key.local) == true){
-      console.log("locale "+Key.local+" already exists. Appending to the correct place")
-      var localid = '#'+Key.local
-      $(localid).append(
-        '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'
-        );
-      } else {
-        $("#main-values").append(
-          '<li id="'+Key.local+'">'+
-          '  <div class="collapsible-header"><i class="material-icons">label_outline</i>'+ Key.local + '</div>'+
-          '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'+
-          '</li>');
-      }
-      keyplaces.push(Key.local);
-  }
-  $('.collapsible').collapsible();
-  keyplacesindex = keyplaces.removeDuplicates();
-
-  for(i=0;i<keyplacesindex.length;i++){
-      $('.collapsible').collapsible('open',i);
-  }
-
-
-}
-
+var initialislodaded = false;
 
 function Feedit() {
   this.signInButton = document.getElementById('submitbt');
@@ -139,47 +29,198 @@ Feedit.prototype.initFirebase = function() {
   // Initiates Firebase auth and listen to auth state changes.
 };
 
-// Fetches and calls displayData of the initial database state
-Feedit.prototype.getData = function(){
-  $("#maindata").append(loading);
-  $("#loadingbar").center();
-
+Feedit.prototype.startHandler = function(){
   var ref = this.database.ref(currentUser.uid);
-    // Attach an asynchronous callback to read the data at our posts reference
-  ref.on("child_added", function(snapshot) {
-    console.log("Initial data obtained:");
-    this.initialdb = snapshot.val();
-    // console.log(this.initialdb);
-    // console.log("Attempting do draw data")
-    db[datacounter] = (this.initialdb);
+  ref.once("value", function(snap) {
+    currentUser.count = snap.numChildren();
+    console.log("INITIAL DATA COUNT:", currentUser.count);
 
-  feedit.displayData2(this.initialdb);
-  // feedit.displayData(0);
-  // feedit.updateData();
-datacounter++;
-    // Check if database is empty for user
-    if(this.initialdb == null){
+    if(currentUser.count == 0){
       $("#maindata").append(
         "<h5 class='center' style='padding-top:50px;'> Não existem dados para esse usuário. Configure o dispositivo de feedback corretamente. </h5>");
     }
-  }, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
+
+    feedit.getData();
   });
 }
 
-Feedit.prototype.updateData = function(){
-  console.log("updateData function ran | updatecounter:"+updatecounter);
-  var ref = this.database.ref(currentUser.uid);
-  ref.limitToLast(1).on("child_added", function(snapshot, prevChildKey) {
-    newEval = snapshot.val();
-    console.log("Last child recieved: ");
-    console.log(newEval);
-    // console.log("Previous child ID:" + prevChildKey);
-  // feedit.displayData(1);
-  updatecounter ++;
-
-});
+Feedit.prototype.onDataLoaded = function(){
+  // change badge counter
+  // $(".badge").attr("class","badge");
 }
+
+// Initialize mainapp
+Feedit.prototype.displayGui = function(){
+  $("#loadingbar").attr("style","");
+  $("#loadingbar").center();
+  $("#header").remove();
+  $("#iconsection").remove();
+  $("#mainshow").removeClass("mainbg");
+  $( "#main-content" ).append(
+  "<div class='center'><h5><b>Painel de Controle</b></h5><h6 style='color:grey;'>" + currentUser.email + " | ID: " + currentUser.uid +  "</h6>" +
+  "<div class='divider'></div></div>"+
+  "<div id='maindata' class='Site-content'></div>");
+  loading = document.getElementById("loadingbar");
+  $("#maindata").append(loading);
+  $("#navbar").css("background-color","rgb(187,41,41)");
+
+  // Draws container for key values
+  $("#maindata").append(
+    '<div class="container">'+
+    '<h5 style="color:gray">Seus locais</h5>'+
+    '<ul id="main-values" class="collapsible" data-collapsible="expandable" >'+
+    '</ul>'+
+    '</div>');
+}
+
+Feedit.prototype.displayData2 = function(data){
+  Key = data;
+
+    if(places.includes(Key.local) == true){ // PREPENDS TO THE LATEST ITEM LIST
+      console.log("locale "+Key.local+" already exists. Appending to the correct place")
+      var localid = '#'+Key.local;
+      var localid_counter_ref = '#counter-'+Key.local;
+      var localchild_id = '#'+Key.local+'>:nth-child(2)';
+      // $("#biblioteca>:nth-child(2)").before("<div>inserted div</div>");
+      $(localchild_id).before(
+      '  <div class="collapsible-body"><span style="padding-left:5%">'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'
+      );
+      $(localid_counter_ref).html($(localid)[0].childElementCount-1);
+
+    } else {  // APPENDS FOR THE FIRST TIME, CREATING THE HEADER
+      $("#main-values").append(
+        '<li id="'+Key.local+'">'+
+        '  <div class="collapsible-header" hasflag="0" id="header-'+Key.local+'"><i class="material-icons">label_outline</i><span id="counter-'+Key.local+'"class="badge">'+1+'</span>'+ Key.local.capitalize() + '</div>'+
+          '  <div class="collapsible-body" id="last-'+Key.local+'"><span style="padding-left:5%">'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'+
+        '</li>');
+    }
+
+    if (initialislodaded == true){ // adds and counts new label
+      newBadge = '<span id="badge-'+Key.local+'" class="new badge green" data-badge-caption="Nova"></span>'
+      var localid_header = '#header-'+Key.local;
+      var localid_badge = '#badge-'+Key.local;
+      var flag = $(localid_header).attr('hasflag');
+      $(localid_header).attr('counter',parseInt($(localid_header).attr('counter'))+1);
+      $(localid_badge).html(parseInt($(localid_header).attr('counter')));
+
+      console.log(flag);
+
+      if(flag == "0"){
+        $(localid_header).append(newBadge);
+        $(localid_header).attr('hasflag','1');
+        $(localid_header).attr('counter','1');
+      }
+    }
+
+    places.push(Key.local);
+    $("#loadingbar").remove();
+    $('.collapsible').collapsible();
+    //Adds aditional data loaded in realtime
+    for(i=0;i<places.removeDuplicates().length;i++){
+        $('.collapsible').collapsible('open',i);
+    }
+}
+
+// Function called to display data onload and every time a new key is pushed to the db
+// Feedit.prototype.displayData = function(kind){
+//   console.log("display data called | updatecounter:"+updatecounter);
+//   if(kind==0){
+//     keyplaces = []
+//       for (key in initialdb) {
+//         if (initialdb.hasOwnProperty(key)) {
+//           // Check for keys that contain different locals;
+//           Key = initialdb[key]
+//           // console.log(Key);
+//           if(keyplaces.includes(Key.local) == true){
+//             console.log("locale "+Key.local+" already exists. Appending to the correct place")
+//             var localid = '#'+Key.local
+//             $(localid).append(
+//               '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'
+//               );
+//             } else {
+//               $("#main-values").append(
+//                 '<li id="'+Key.local+'">'+
+//                 '  <div class="collapsible-header"><i class="material-icons">label_outline</i>'+ Key.local.capitalize() + '</div>'+
+//                 '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'+
+//                 '</li>');
+//             }
+//             keyplaces.push(Key.local);
+//         }
+//     }
+//     $("#loadingbar").remove();
+//     console.log("Initial data written to UI successfully");
+//     $('.collapsible').collapsible();
+//     // Adds aditional data loaded in realtime
+//   } else if (kind==1 && updatecounter>=1){
+//     console.log("updatecounter >= 1!");
+//     Key = newEval;
+//     if(keyplaces.includes(Key.local) == true){
+//       console.log("locale "+Key.local+" already exists. Appending to the correct place")
+//       var localid = '#'+Key.local
+//       $(localid).append(
+//         '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'
+//         );
+//       } else {
+//         $("#main-values").append(
+//           '<li id="'+Key.local+'">'+
+//           '  <div class="collapsible-header"><i class="material-icons">label_outline</i>'+ Key.local + '</div>'+
+//           '  <div class="collapsible-body"><span>'+ Key.nota +' | ' + Key.date + ' | ' + Key.time +'</span></div>'+
+//           '</li>');
+//       }
+//       keyplaces.push(Key.local);
+//   }
+//   $('.collapsible').collapsible();
+//   keyplacesindex = keyplaces.removeDuplicates();
+//
+//   for(i=0;i<keyplacesindex.length;i++){
+//       $('.collapsible').collapsible('open',i);
+//   }
+//
+//
+// }
+
+// Fetches and calls displayData of the data
+Feedit.prototype.getData = function(){
+  var ref = this.database.ref(currentUser.uid);
+
+  // Attach an asynchronous callback to read data as soon as it is added to the database
+  ref.on("child_added", function(snapshot) {
+    this.key = snapshot.val();
+
+    db[datacounter] = (this.key);
+    datacounter++;
+
+    // displays data as soon as it is obtained
+    feedit.displayData2(this.key);
+
+    // Check if initial data has been loaded fully;
+    if(datacounter === currentUser.count){
+      console.log("Initial data loading complete - launching initialislodaded as True");
+      initialislodaded = true;
+
+      feedit.onDataLoaded();
+      // Materialize.showStaggeredList('#main-data');// run display animation;
+    }
+
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+}
+
+// Feedit.prototype.updateData = function(){
+//   console.log("updateData function ran | updatecounter:"+updatecounter);
+//   var ref = this.database.ref(currentUser.uid);
+//   ref.limitToLast(1).on("child_added", function(snapshot, prevChildKey) {
+//     newEval = snapshot.val();
+//     console.log("Last child recieved: ");
+//     console.log(newEval);
+//     // console.log("Previous child ID:" + prevChildKey);
+//   // feedit.displayData(1);
+//   updatecounter ++;
+//
+// });
+// }
 
 Feedit.prototype.checkSetup = function() {
   if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
@@ -220,7 +261,7 @@ Feedit.prototype.signOut = function() {
 };
 
 Feedit.prototype.onAuthStateChanged = function(user) {
-  console.log("Current user state:");
+  console.log("User auth state:");
   console.log(user);
   currentUser = user;
   if (user) { // User is signed in!
@@ -231,36 +272,30 @@ Feedit.prototype.onAuthStateChanged = function(user) {
     document.getElementById("topuserdisplay").innerHTML = user.email;
     document.getElementById("nameshow").innerHTML = user.email;
 
-
-    if(initalstate == 0){
-      Materialize.toast("Usuário autenticado com sucesso!",4000);
-    }
-
-    initalstate = 1;
+    feedit.displayGui();
+    Materialize.toast("Usuário autenticado com sucesso!",4000);
+    feedit.startHandler();
 
     // Começa o display da GUI de usuário:
-    if(initalstate == 1){
-      feedit.displayGui();
-    }
 
   } else { // User is signed out!
     // $("#topuserdisplay-off").css("visibility","visible");
     $("#topuserdisplay-off").css("display","block");
     console.log("User has/is signed out!");
-    initalstate = 0;
+    initialstate = 0;
   }
 };
 
-function retrieve(){
-  $("#submitbt").hide();
-  $("#loadingbar").show();
-  return firebase.database().ref('/').once('value').then(function(snapshot) {
-    var username = snapshot.val();
-    window.username = username;
-    // document.write(username)
-    // ...
-  });
-}
+// function retrieve(){
+//   $("#submitbt").hide();
+//   $("#loadingbar").show();
+//   return firebase.database().ref('/').once('value').then(function(snapshot) {
+//     var username = snapshot.val();
+//     window.username = username;
+//     // document.write(username)
+//     // ...
+//   });
+// }
 
 function login(){
   var valemail = $('#useremail').attr('class');
@@ -270,15 +305,15 @@ function login(){
   if(valemail == "validate valid"){
     feedit.userName = $("#useremail").val();
     feedit.userPass = $("#userpassword").val();
-    console.log("Loggin in with credentials:" + feedit.userName + "|" + feedit.userPass);
+    console.log("Logging in with credentials:" + feedit.userName + "|" + feedit.userPass);
     feedit.signIn();
-    if(initalstate == 0){
+    if(initialstate == 0){
       $("#submitbt").hide();
       $("#loadingbar").show();
     }
 
   } else {
-    Materialize.toast("Por favor preencha os campos corretamente",2000)
+    Materialize.toast("Por favor preencha os campos corretamente",2000);
   }
 };
 
